d52ce4556b09575b8644b460f0db11746d9968b1
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-02-23 17:35:12 -0300

    finished auth header

--- app/src/main/java/br/senai/sp/jandira/petsaudeapp/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/petsaudeapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package br.senai.sp.jandira.petsaudeapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -11,11 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.petsaudeapp.components.AuthHeaderTitle
 import br.senai.sp.jandira.petsaudeapp.ui.theme.PetSaudeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,6 +38,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Global() {
+	LoginHeader()
+}
+
+@Composable
+fun LoginHeader() {
+	val context = LocalContext.current
+
 	Column(
 		modifier = Modifier
 			.fillMaxSize()
@@ -47,26 +59,13 @@ fun Global() {
 			verticalArrangement = Arrangement.SpaceBetween,
 			horizontalAlignment = Alignment.CenterHorizontally
 		) {
-			Column(
-				modifier = Modifier.fillMaxWidth(),
-				horizontalAlignment = Alignment.CenterHorizontally
-			) {
-				Text(
-					"Bem-vindo de volta!",
-					color = Color.Black,
-					fontSize = 24.sp,
-					fontWeight = FontWeight.Bold
-				)
-				Text(
-					"Por favor, insira suas informações abaixo",
-					color = Color.LightGray,
-					fontSize = 14.sp,
-					fontWeight = FontWeight.Normal
-				)
-			}
+			AuthHeaderTitle(title = "Bem vindo de volta!", subtitle = "Por favor, insira suas informações abaixo")
 			Spacer(modifier = Modifier.height(32.dp))
 			Button(
-				onClick = {/* TODO */ },
+				onClick = {
+//				TODO: FIREBASE AUTHENTICATION
+					Toast.makeText(context, "Button Click", Toast.LENGTH_SHORT).show()
+				},
 				modifier = Modifier.size(60.dp),
 				shape = RoundedCornerShape(50),
 				colors = ButtonDefaults.buttonColors(Color.LightGray)
@@ -78,8 +77,32 @@ fun Global() {
 				)
 			}
 			Spacer(modifier = Modifier.height(32.dp))
-			Row() {
-				Text("Ou use o e-mail")
+			Row(
+				modifier = Modifier
+					.fillMaxWidth(),
+				horizontalArrangement = Arrangement.Center,
+				verticalAlignment = Alignment.CenterVertically
+			) {
+				Spacer(
+					modifier = Modifier
+						.height(1.dp)
+						.width(100.dp)
+						.background(color = Color.LightGray)
+				)
+				Text(
+					text = "Ou use o e-mail",
+					modifier = Modifier.padding(start = 10.dp, end = 10.dp),
+					color = Color.LightGray,
+					fontSize = 14.sp,
+					fontWeight = FontWeight.Normal,
+					textAlign = TextAlign.Center
+				)
+				Spacer(
+					modifier = Modifier
+						.height(1.dp)
+						.width(100.dp)
+						.background(color = Color.LightGray)
+				)
 			}
 		}
 	}
---
+++ app/src/main/java/br/senai/sp/jandira/petsaudeapp/components/AuthHeaderTitle.kt
@@ -0,0 +1,35 @@
+package br.senai.sp.jandira.petsaudeapp.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AuthHeaderTitle(title: String, subtitle: String) {
+	Column(
+		modifier = Modifier.fillMaxWidth(),
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		Text(
+			text = title,
+			color = Color.Black,
+			fontSize = 32.sp,
+			fontWeight = FontWeight.Bold,
+			textAlign = TextAlign.Center
+		)
+		Text(
+			text = subtitle,
+			color = Color.LightGray,
+			fontSize = 16.sp,
+			fontWeight = FontWeight.Normal,
+			textAlign = TextAlign.Center
+		)
+	}
+}
