c63ada979aa3040f0fba96c0127f8e7697b2d5ff
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-02-27 08:42:53 -0300

    Statusbar color changed

--- app/src/main/java/br/senai/sp/jandira/petsaudeapp/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/petsaudeapp/MainActivity.kt
@@ -1,7 +1,8 @@
 package br.senai.sp.jandira.petsaudeapp
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
@@ -20,7 +21,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -30,6 +32,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import br.senai.sp.jandira.petsaudeapp.components.AuthHeaderTitle
 import br.senai.sp.jandira.petsaudeapp.components.BottomMessage
 import br.senai.sp.jandira.petsaudeapp.ui.theme.PetSaudeAppTheme
@@ -37,6 +40,7 @@ import br.senai.sp.jandira.petsaudeapp.ui.theme.PetSaudeAppTheme
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		WindowCompat.setDecorFitsSystemWindows(window, false)
 		setContent {
 			PetSaudeAppTheme {
 				Surface(
@@ -49,6 +53,26 @@ class MainActivity : ComponentActivity() {
 		}
 	}
 }
+
+//@Composable
+//fun StatusBar(window: Window) {
+//	MaterialTheme {
+//		window.statusBarColor = MaterialTheme.colors.surface.toArgb()
+//		window.navigationBarColor = MaterialTheme.colors.surface.toArgb()
+//
+//		@Suppress("DEPRECATION")
+//		if (MaterialTheme.colors.surface.luminance() > 0.5f) {
+//			window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+//							View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+//		}
+//
+//		@Suppress("DEPRECATION")
+//		if (MaterialTheme.colors.surface.luminance() > 0.5f) {
+//			window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+//							View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+//		}
+//	}
+//}
 
 @Composable
 fun GlobalLogin() {
--- app/src/main/java/br/senai/sp/jandira/petsaudeapp/ui/theme/Theme.kt
+++ app/src/main/java/br/senai/sp/jandira/petsaudeapp/ui/theme/Theme.kt
@@ -1,44 +1,52 @@
 package br.senai.sp.jandira.petsaudeapp.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
+import android.app.Activity
+import  androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 
 private val DarkColorPalette = darkColors(
-        primary = Green400,
-        primaryVariant = Green500,
-        secondary = Blue600
+	primary = Green400,
+	primaryVariant = Green500,
+	secondary = Blue600,
+	background = White100,
+  onBackground = Black700
 )
 
 private val LightColorPalette = lightColors(
-        primary = Green400,
-        primaryVariant = Green500,
-        secondary = Blue600
-
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+	primary = Green400,
+	primaryVariant = Green500,
+	secondary = Blue600,
+	background = White100,
+	onBackground = Black700
 )
 
 @Composable
 fun PetSaudeAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+	val colors = if (darkTheme) {
+		DarkColorPalette
+	} else {
+		LightColorPalette
+	}
 
-    MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
-    )
+	val view = LocalView.current
+  if (!view.isInEditMode) {
+    SideEffect {
+      val window = (view.context as Activity).window
+      window.statusBarColor = colors.secondary.toArgb()
+      window.navigationBarColor = colors.onBackground.toArgb()
+    }
+  }
+
+	MaterialTheme(
+		colors = colors,
+		typography = Typography,
+		shapes = Shapes,
+		content = content
+	)
 }
